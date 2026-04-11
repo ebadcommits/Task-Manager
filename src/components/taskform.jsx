da194d43a05ff6a267adba138f66cc0ff7fac7b6
ebad sheikh <ibbeescode@gmail.com>
2026-04-12 03:03:54 +0500

    Implement TaskForm component with state management and form submission

--- src/components/taskform.jsx
+++ src/components/taskform.jsx
@@ -1,8 +1,26 @@
-function TaskForm() {
+import {usestate} from "react"
+
+
+function TaskForm({onAddTask}) {
+  const [text, setText] = usestate("")
+
+  function handleSubmit(e){
+    e.preventDefault()
+    if (!text.trim()) return
+
+    onAddTask(text)
+    setText("")
+  }
   return (
-    <form>
-      <input type="text" placeholder="Enter a task..." />
-      <button type="submit">Add Task</button>
+    <form onSubmit={handleSubmit} className="flex gap-2">
+      <input
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      type="text" 
+      placeholder="Enter a task..."
+      className="border p-2 rounded w-full" />
+      <button type="submit"
+      className="bg-blue-500 text-white px-4 py-2 rounded">Add Task</button>
     </form>
   );
 }
