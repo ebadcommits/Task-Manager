d4e435e65f8ae14f6d3516ba33a27fe2b070b849
ebad sheikh <ibbeescode@gmail.com>
2026-04-12 03:44:44 +0500

    Refactor TaskItem component for improved styling and structure

--- src/components/taskitem.jsx
+++ src/components/taskitem.jsx
@@ -1,8 +1,8 @@
 function TaskItem() {
   return (
-    <div>
-      <span>Sample Task</span>
-      <button>Delete</button>
+    <div className="flex justify-between items-centre bg-gray-100 p-3 rounded">
+      <span>{TaskItem.text}</span>
+      <button className="text-red-500">Delete</button>
     </div>
   );
 }
