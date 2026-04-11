82a3f48cb3e1ff8123addbd230519a5cd3dde509
ebad sheikh <ibbeescode@gmail.com>
2026-04-12 03:44:36 +0500

    Implement TaskList component to display tasks or a message when no tasks are available

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,7 +1,15 @@
-function TaskList() {
+import TaskItem from "./taskitem";
+
+function TaskList({tasks}) {
+  if(tasks.length === 0) {
+    return <p className="text-gray-500 mt-5">No tasks yet</p>
+  }
+
   return (
-    <div>
-      <p>No tasks yet</p>
+    <div className="mt-5 flex flex-col gap-3">
+      {tasks.map((task) =>(
+        <TaskItem Key={task.id} task={task} />
+      ))}
     </div>
   );
 }
