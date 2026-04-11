6c3bc242eaed0cdf54444a91577b4438b87be520
ebad sheikh <ibbeescode@gmail.com>
2026-04-12 03:17:23 +0500

    Implement task management functionality with addTask method and state handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import {useState} from "react"
 import Header from "./components/header";
 import TaskForm from "./components/taskform";
 import TaskList from "./components/tasklist";
 
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+
+  function addTask(text){
+    const newTask = {
+      id: Date.now(),
+      text,
+      completed: false,
+    }
+    setTasks([...tasks, newTask])
+  }
+
   return (
-   <div>
+   <div className="container mx-auto max-w-lg mt-10 p-4">
       <Header />
-      <TaskForm />
-      <TaskList />
+      <TaskForm onAddTask={addTask}/>
+      <TaskList tasks={tasks} />
    </div>
   );
 }
